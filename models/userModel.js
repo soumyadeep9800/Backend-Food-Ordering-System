99225d89ca51d99ea959047bac6fead357cf9207
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-04-08 13:28:14 +0530

    add userModel

---
+++ models/userModel.js
@@ -0,0 +1,61 @@
+const mongoose=require('mongoose');
+const bcrypt=require('bcrypt');
+
+const userSchema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase: true,
+        trim: true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:Number,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    cart:[
+        {
+            itemName: String,
+            quantity: Number,
+            price: Number
+        }
+    ]
+});
+
+userSchema.pre('save',async function(next){
+    const user=this;
+    if(!user.isModified('password')) return next();
+    try {
+        const salt=await bcrypt.genSalt(10);
+        const hashPassword=await bcrypt.hash(user.password,salt);
+        user.password=hashPassword;
+        next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+userSchema.methods.comparePassword=async function(userPassword){
+    try {
+        const isMatch=await bcrypt.compare(userPassword,this.password);
+        return isMatch;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const User=mongoose.model('User',userSchema);
+module.exports=User;

10e9ffb476b03e4385393be649c98ac457b98295
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-04-10 12:11:55 +0530

    add search restaurant route

--- routes/restaurantService.js
+++ routes/restaurantService.js
@@ -14,4 +14,21 @@ router.get('/',jwtAuthMiddleware,async(req,res)=>{
     }
 });
 
+router.get('/search', jwtAuthMiddleware, async (req, res) => {
+    try {  //GET http://localhost:3000/restaurant/search?name=domino
+        const name = req.query.name;  // <-- correct way
+        const result = await Restaurant.find({name: { $regex: name, $options: 'i' }}); // i => ignore case
+        if(result.length === 0){
+            console.log('not available');
+            return res.status(404).json({ message: 'Not Available' });
+        }
+        console.log(result);
+        res.status(200).json({ result });
+    } catch (error) {
+        console.log('Internal Server Error');
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
 module.exports=router;

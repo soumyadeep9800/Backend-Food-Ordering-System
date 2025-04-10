7ad203ab100bcb2d1b16cdcf86af82470e999b19
soumyadeep9800 <152770412+soumyadeep9800@users.noreply.github.com>
2025-04-10 18:04:07 +0530

    view cart route

--- routes/cartController.js
+++ routes/cartController.js
@@ -19,6 +19,17 @@ router.post('/',jwtAuthMiddleware,async(req,res)=>{
     }
 });
 
-
+router.get('/',jwtAuthMiddleware,async(req,res)=>{
+    try {
+        const userID=req.user.id;
+        const user=await User.findById(userID);
+        if(!user) return res.status(404).json({message:'user not found'});
+        console.log(user.cart);
+        res.status(200).json({ cart: user.cart });
+    } catch (error) {
+        console.log('Failed to view cart',error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports=router;
